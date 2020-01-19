b5c8c1c79844a743459e7938f7b2bec620d2b1ae
Ivan Carvalho <ivan.carvalho@alumni.ubc.ca>
2020-01-18 18:33:51 -0800

    Connect to MongoDB Atlas from Google Cloud

--- add_task/main.py
+++ add_task/main.py
@@ -1,12 +1,24 @@
 import json
+import os
 import pymongo
+
+client = pymongo.MongoClient(
+    "mongodb+srv://{}:{}@{}".format(
+        os.getenv("MONGO_USER"), os.getenv("MONGO_PASSWORD"), os.getenv("MONGO_URL")
+    )
+)
+db = client.motivdb
 
 
 def add_task(request):
     try:
         request_json = request.get_json(silent=True)
         print("HELLO WORLD: {}".format(request_json))
-        answer = {"debug": request_json, "message": "Task has been added"}
+        answer = {
+            "debug": request_json,
+            "message": "Task has been added",
+            "mongoUser": os.getenv("MONGO_USER"),
+        }
         return json.dumps(answer)
     except:
         print("BYE WORLD")
